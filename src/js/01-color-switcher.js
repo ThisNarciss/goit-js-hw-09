adf58e900d15b7b2a443356effe18db6a2797003
ThisNarciss <tyomaxxx000@gmail.com>
2022-12-06 11:47:28 +0200

    complete task 1 hw 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  refs.btnStart.setAttribute('disabled', 'true');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  refs.btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+}
